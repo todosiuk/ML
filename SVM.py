da58a10e0c922bc6db87a91e582f3e085e0129d9
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2018-08-25 18:28:01 +0300

    svm

---
+++ SVM.py
@@ -0,0 +1,44 @@
+# To help us perform math operations
+import numpy as np
+
+# Input data - Of the form [X value, Y value, Bias term]
+X = np.array([
+    [-2, 4, -1],
+    [4, 1, -1],
+    [1, 6, -1],
+    [2, 4, -1],
+    [6, 2, -1],
+])
+
+# Associated output labels - First 2 examples are labeled '-1' and last 3 are labeled '+1'
+y = np.array([-1, -1, 1, 1, 1])
+
+
+def svm_sgd_plot(X, Y):
+    # Initialize our SVMs weight vector with zeros (3 values)
+    w = np.zeros(len(X[0]))
+    # The learning rate
+    eta = 1
+    # how many iterations to train for
+    epochs = 100000
+    # store misclassifications so we can plot how they change over time
+    errors = []
+
+    # training part, gradient descent part
+    for epoch in range(1, epochs):
+        error = 0
+        for i, x in enumerate(X):
+            # misclassification
+            if (Y[i] * np.dot(X[i], w)) < 1:
+                # misclassified update for ours weights
+                w = w + eta * ((X[i] * Y[i]) + (-2 * (1 / epoch) * w))
+                error = 1
+            else:
+                # correct classification, update our weights
+                w = w + eta * (-2 * (1 / epoch) * w)
+        errors.append(error)
+
+    return w
+
+
+svm_sgd_plot(X, y)
